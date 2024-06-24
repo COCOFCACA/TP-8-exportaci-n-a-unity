c30beab286ca404bfba7f32f34bb5830347c6e37
COCOFCACA <nicohfernandez21@gmail.com>
2024-06-24 14:16:32 -0300

    .

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,23 +5,44 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     int conteo = 1;
-    public GameObject[] Aparición;
+    public GameObject[] arrayobjectos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (GameObject modelo in arrayobjectos)
+        {
+            modelo.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            conteo += 1;
+            if (conteo > arrayobjectos.Length - 1)
+            {
+                conteo = 0;
+            }
+            foreach (GameObject modelo in arrayobjectos)
+            {
+                modelo.SetActive(false);
+            }
+            arrayobjectos[conteo].SetActive(true);
+        }
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            conteo++;
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-
+            conteo -= 1;
+            if (conteo < 0)
+            {
+                conteo = arrayobjectos.Length - 1;
+            }
+            foreach (GameObject modelo in arrayobjectos)
+            {
+                modelo.SetActive(false);
+            }
+            arrayobjectos[conteo].SetActive(true);
         }
     }
 }
